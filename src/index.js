68fa216d75c9319e67876af911de75fd42091f66
xoxlushka1337 <119114997+xoxlushka1337@users.noreply.github.com>
2023-02-28 01:28:28 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 // You should implement your task here.
 
 module.exports = function towelSort(matrix) {
+    if (matrix === undefined) {
+        return [];
+    }
     return matrix.reduce((acc, cur, i) => {
         cur.sort((a, b) => (!(i & 1) ? a - b : b - a)).map((e) => acc.push(e));
         return acc;
